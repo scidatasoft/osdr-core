34ed4b464cebabe0b1aabbf5c7c187575c3a79ec
Aleksey <aleksey.wheatman@gmail.com>
2019-07-06 11:23:56 -0400

    fix broken test

---
+++ Sds.Osdr.IntegrationTests/FluentAssersions/Files/MicroscopyFileAssersions.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using FluentAssertions.Collections;
+using Leanda.Microscopy.Domain;
+using Sds.Osdr.Generic.Domain;
+using Sds.Osdr.Generic.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sds.Osdr.IntegrationTests.FluentAssersions
+{
+    public static class MicroscopyFileAssersionsExtensions
+    {
+        public static void EntityShouldBeEquivalentTo(this GenericDictionaryAssertions<string, object> assertions, MicroscopyFile file)
+        {
+            assertions.Subject.ShouldAllBeEquivalentTo(new Dictionary<string, object>
+            {
+                { "_id", file.Id},
+                { "Blob", new Dictionary<string, object>() {
+                    { "_id", file.BlobId},
+                    { "Bucket", file.Bucket },
+                    { "Length", file.Length },
+                    { "Md5", file.Md5 }
+                } },
+                { "SubType", FileType.Microscopy.ToString() },
+                { "OwnedBy", file.OwnedBy },
+                { "CreatedBy", file.CreatedBy },
+                { "CreatedDateTime", file.CreatedDateTime.UtcDateTime},
+                { "UpdatedBy", file.UpdatedBy },
+                { "UpdatedDateTime", file.UpdatedDateTime.UtcDateTime},
+                { "ParentId", file.ParentId },
+                { "Name", file.FileName },
+                { "Status", file.Status.ToString() },
+                { "Version", file.Version },
+                { "Images", file.Images.Select(i => new Dictionary<string, object> {
+                    { "_id", i.Id },
+                    { "Bucket", file.Bucket },
+                    { "Height", i.Height },
+                    { "Width", i.Height },
+                    { "MimeType", i.MimeType },
+                    { "Scale", i.GetScale() }
+                })},
+                { "Metadata", file.Metadata }
+            });
+        }
+    }
+}
