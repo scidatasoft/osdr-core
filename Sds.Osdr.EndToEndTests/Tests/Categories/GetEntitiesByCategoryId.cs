d0d64cf67ce70975ba4a5d1010659cb78040e5db
aleksander_sidorenko <aleksander.sidorenko@ormdigital.com>
2019-11-15 17:10:21 +0200

    added one more test case

---
+++ Sds.Osdr.EndToEndTests/Tests/Categories/GetEntitiesByCategoryId.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Leanda.Categories.Domain.ValueObjects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Sds.Osdr.IntegrationTests;
+using Sds.Osdr.IntegrationTests.Traits;
+using Sds.Osdr.WebApi.IntegrationTests;
+using Sds.Osdr.WebApi.IntegrationTests.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sds.Osdr.EndToEndTests.Tests.Categories
+{
+    [Collection("OSDR Test Harness")]
+    public class GetEntitiesByCategoryId : OsdrWebTest, IClassFixture<GetCategoriesIdsByEntityIdFixture>
+    {
+        private Guid CategoryId;
+        public Guid BlobId { get; set; }
+        public Guid FileId { get; set; }
+
+
+        public GetEntitiesByCategoryId(OsdrTestHarness harness, ITestOutputHelper output, GetCategoriesIdsByEntityIdFixture fixture) : base(harness, output)
+        {
+            CategoryId = fixture.CategoryId;
+            BlobId = fixture.BlobId;
+            FileId = fixture.FileId;
+        }
+
+        [Fact, WebApiTrait(TraitGroup.All, TraitGroup.Categories)]
+        public async Task GetCategoriesIdsByEntityIdTest()
+        {
+            var categoriesIds = await JohnApi.ReadJsonAsync<IEnumerable<string>>($"/api/categoryentities/categories/{CategoryId}");
+            categoriesIds.Any(x => x == CategoryId.ToString()).Should().BeTrue();
+        }
+    }
+}
